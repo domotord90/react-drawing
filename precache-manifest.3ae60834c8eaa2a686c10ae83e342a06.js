b0d11d7665f563394d43baeb96f049a4424bc006
domotord90 <jazzmaster@freemail.com>
2019-11-16 14:53:34 +0100

    Updates

---
+++ precache-manifest.3ae60834c8eaa2a686c10ae83e342a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04128c375a661d8af3acd3e556009af",
+    "url": "/react-drawing/index.html"
+  },
+  {
+    "revision": "2556543adade586e6b2e",
+    "url": "/react-drawing/static/css/main.41412406.chunk.css"
+  },
+  {
+    "revision": "ab7a6c8555fe4a3a5a14",
+    "url": "/react-drawing/static/js/2.43109d01.chunk.js"
+  },
+  {
+    "revision": "2556543adade586e6b2e",
+    "url": "/react-drawing/static/js/main.9fc328fe.chunk.js"
+  },
+  {
+    "revision": "e077dcdc699497c5653e",
+    "url": "/react-drawing/static/js/runtime-main.06e82327.js"
+  },
+  {
+    "revision": "cf9bff09a497cf365f7ec5134727a729",
+    "url": "/react-drawing/static/media/eraser.cf9bff09.svg"
+  },
+  {
+    "revision": "4619d2deb458974ee263a457d06fd994",
+    "url": "/react-drawing/static/media/pencil.4619d2de.svg"
+  }
+]);
